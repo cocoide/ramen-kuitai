8affa97da285ba5cf54166a76509142b60d96ab1
{cocoide} <{cocoide.com@gmail.com}>
2022-12-09 15:32:06 +0900

    Test dynamic pages in nextjs13 with todo API

--- src/app/todos/[slug]/page.tsx
+++ src/app/todos/[slug]/page.tsx
@@ -1,11 +1,30 @@
-"use client"
+import { Todo } from '../../../../types';
 
-const page = () => {
+// https://github.com/michaelrambeau/bestofjs-nextjs13/blob/main/app/projects/%5Bslug%5D/page.tsx
+type PageProps = {
+    params: {
+        slug: string;
+    };
+};
+
+const page = async ({ params }: PageProps) => {
+    const { slug } = params;
+    const todo = await fetchTodo(slug);
+
 
     return (
         <div className="">
-            <div>page</div>
+            <div>params:{params.slug}</div>
+            <div>id:{todo.id}</div>
+            <div> title:{todo.title}</div>
+            <div> uid:{todo.userId}</div>
         </div>
     )
 }
 export default page
+
+const fetchTodo = async (slug: string) => {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/todos/${slug}`);
+    const todo = await res.json();
+    return todo;
+};
