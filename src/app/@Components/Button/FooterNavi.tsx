306d73903cde89761f09d66c98a8e26bc6565d2d
{cocoide} <{cocoide.com@gmail.com}>
2022-12-20 02:06:06 +0900

    Fixed detailed design

--- src/app/@Components/Button/FooterNavi.tsx
+++ src/app/@Components/Button/FooterNavi.tsx
@@ -1,14 +1,15 @@
 import Link from 'next/link'
-import { HomeIcon, MapIcon, PlusCircleIcon, UserIcon } from '@heroicons/react/24/outline'
+import { BellIcon, HomeIcon, MapIcon, PlusCircleIcon, UserIcon } from '@heroicons/react/24/outline'
 
 const FooterNavi = () => {
     return (
-        <div className="bg-white h-15 fixed inset-x-0 bottom-0 w-full flex justify-center p-2 place-items-center
-        space-x-10 border-t-2 border-gray-200">
-            <Link href={"/"}><HomeIcon className="h-10" /></Link>
-            <Link href={"/user/profile"}><UserIcon className='h-10' /></Link>
-            <Link href={"/ramens"}><MapIcon className='h-10' /></Link>
-            <Link href={"/review"}><PlusCircleIcon className='h-10' /></Link>
+        <div className="bg-[#FFAF19] h-15 fixed inset-x-0 bottom-0 w-full flex justify-center place-items-center p-2
+        space-x-7 border-t-2 border-white shadow-md">
+            <Link href={"/"}><HomeIcon className="h-10 text-white" /></Link>
+            <Link href={"/user/profile"}><UserIcon className='h-10 text-white' /></Link>
+            <Link href={"/review"}><PlusCircleIcon className='h-10 text-white' /></Link>
+            <Link href={"/notice"}><BellIcon className='h-10 text-white' /></Link>
+            <Link href={"/ramens"}><MapIcon className='h-10 text-white' /></Link>
         </div>
     )
 }
--- src/app/@Components/SearchRamen/index.tsx
+++ src/app/@Components/SearchRamen/index.tsx
@@ -2,13 +2,13 @@ import { CheckIcon, ChevronUpDownIcon, MagnifyingGlassIcon } from '@heroicons/re
 const SearchRamen = () => {
     return (
         <>
-        <form className='flex bg-white rounded-full drop-shadow-xl z-50 p-1 w-auto'>
+            <form className='flex bg-white rounded-full drop-shadow-md z-50 p-1 w-auto ring-2 ring-gray-200'>
             <input className="focus:ring-0 border-none
              flex justify-center
-            rounded-full  w-[350px] md:w-[600px] lg:w-[800px]  h-15 lg:h-17 p-2"
-                placeholder="Enter the keyword about Ramen ..." />
+            rounded-full  w-[350px] md:w-[600px] lg:w-[800px]  h-15 lg:h-17 p-2 text-center"
+                    placeholder="キーワード｜(種類・駅名・店名)を入力" />
             <button className="focus:ring-0 border-none">
-                <MagnifyingGlassIcon className="h-8 w-8 border-l-2 ml-1 border-[#FFAF19] text-[#FFAF19]" />
+                    <MagnifyingGlassIcon className="h-9 w-9 border-l-2 ml-1 px-1 border-[#FFAF19] text-[#FFAF19]" />
             </button>
         </form>
         </>
