ea0450541f0eb3601cd8ca0ffbcd949adcc9177a
{cocoide} <{cocoide.com@gmail.com}>
2022-12-21 02:03:01 +0900

    Fixed auth error

--- src/libs/server/auth.ts
+++ src/libs/server/auth.ts
@@ -12,35 +12,35 @@ export const authOptions={
     adapter: PrismaAdapter(prisma),
     secret: process.env.NEXT_PUBLIC_SECRET,
     
-    // callbacks: {
-    //     async session({ token, session }) {
-    //       if (token) {
-    //         session.user.id = token.id
-    //         session.user.name = token.name
-    //         session.user.email = token.email
-    //         session.user.image = token.picture
-    //       }
+    callbacks: {
+        async session({ token, session }) {
+          if (token) {
+            session.user.id = token.id
+            session.user.name = token.name
+            session.user.email = token.email
+            session.user.image = token.picture
+          }
 
-    //       return session
-    //     },
-    //     async jwt({ token, user }) {
-    //       const prismaUser = await prisma.user.findFirst({
-    //         where: {
-    //           email: token.email,
-    //         },
-    //       })
+          return session
+        },
+        async jwt({ token, user }) {
+          const prismaUser = await prisma.user.findFirst({
+            where: {
+              email: token.email,
+            },
+          })
     
-    //       if (!prismaUser) {
-    //         token.id = user.id
-    //         return token
-    //       }
+          if (!prismaUser) {
+            token.id = user.id
+            return token
+          }
     
-    //       return {
-    //         id: prismaUser.id,
-    //         name: prismaUser.name,
-    //         email: prismaUser.email,
-    //         picture: prismaUser.image,
-    //       }
-    //     },
-    // },
+          return {
+            id: prismaUser.id,
+            name: prismaUser.name,
+            email: prismaUser.email,
+            picture: prismaUser.image,
+          }
+        },
+    },
 }
