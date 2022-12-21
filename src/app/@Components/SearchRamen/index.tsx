f38ea38fa17ac02f6a90f40b9f749106c16a2338
{cocoide} <{cocoide.com@gmail.com}>
2022-12-21 22:15:44 +0900

    Try to fix erro

--- src/app/@Components/SearchRamen/index.tsx
+++ src/app/@Components/SearchRamen/index.tsx
@@ -2,7 +2,7 @@ import { MagnifyingGlassIcon } from '@heroicons/react/20/solid'
 const SearchRamen = () => {
     return (
         <>
-            <form className='flex rounded-full shadow-inner z-50 p-1 w-auto bg-gray-100 z-20'>
+            <form className='flex rounded-full shadow-inner p-1 w-auto bg-gray-100 z-20'>
                 <input className="focus:ring-0 border-none bg-gray-100
              flex justify-center
             rounded-full  w-[350px] md:w-[600px] lg:w-[800px]  h-15 lg:h-17 p-2 text-center"
