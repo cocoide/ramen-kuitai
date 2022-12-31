04077edec6e5967b1b4d3732f8f5a3691c611e09
{cocoide} <{cocoide.com@gmail.com}>
2022-12-31 12:34:55 +0900

    Change header color

--- src/app/@Components/@Header/index.tsx
+++ src/app/@Components/@Header/index.tsx
@@ -2,7 +2,7 @@
 import Link from "next/link";
 import React from "react";
 import "/styles/globals.css";
-import { Bars4Icon, PlusCircleIcon, UserCircleIcon } from '@heroicons/react/24/outline'
+import { Bars4Icon, MagnifyingGlassIcon, PlusCircleIcon, UserCircleIcon, BellIcon } from '@heroicons/react/24/outline'
 import { useSetRecoilState } from 'recoil';
 import { isDrawerOpen } from '../../../@types/models/Drawer';
 import Image from 'next/image';
@@ -20,31 +20,43 @@ function Header() {
 
     return (
         <header className="z-50 p-2 bg-white flex justify-between
-        place-items-center border-[#FFAF19]">
+        place-items-center">
 
 
             <Link href="/" className="flex justify-center
-            items-center font-bold text-3xl text-[#FFAF19] m-2"
-            > <Image src="/ramen-kuitai.svg" alt="ramen-kuitai" width={100} height={50} className=""
+            items-center font-bold text-3xl text-[#e0d5c1] m-2"
+            > <Image src="/ramen-kuitai-logo.svg" alt="ramen-kuitai" width={110} height={60} className=""
                 /></Link>
             {/* Logo */}
 
             <div className="flex place-items-center">
 
 
+                <Link href={"/search"}><MagnifyingGlassIcon className='h-10 text-[#e0d5c1]  hover:scale-95 duration-300' /></Link>
+                {/* Search button */}
+
+                <Link href={"/notice"}><BellIcon className='h-10 text-[#e0d5c1]  hover:scale-95 duration-300 ml-3' /></Link>
+                {/* Notice button */}
+
+
+                <div className="place-items-center space-x-3 hidden lg:inline-flex">
+
+
+                    <Link href={"/review"}><PlusCircleIcon className='h-10 text-[#e0d5c1]  hover:scale-95 duration-300 ml-3' /></Link>
+                    {/* Review button */}
+
                 {(isLoadingUser) ? <div className="w-10 h-10 bg-gray-300 animate-pulse rounded-full" />
                         : user ? <AvaterMenu />
                         : <button onClick={() => OpenLoginModal(true)} className='text-[#FFAF19] ring-none'
                         ><UserCircleIcon className="h-10" /></button>}
                 {/* User menu */}
+                </div>
 
-                        <Link href={"/review"}><PlusCircleIcon className='h-10 text-[#FFAF19] hover:scale-95 duration-300
-                    mx-3' /></Link>
-                        {/* Review button */}
+
                         
-                <button className="w-8 mr-3">
+                <button className="w-8 mx-3">
                     <Bars4Icon
-                        className='h-10 w-10 text-[#FFAF19]'
+                        className='h-10 w-10 text-[#e0d5c1]'
                         onClick={() => OpenDrawer(true)}
                     /></button>
                 {/* Menu button */}
--- src/app/@Components/Button/FooterNavi.tsx
+++ src/app/@Components/Button/FooterNavi.tsx
@@ -3,14 +3,14 @@ import { BellIcon, HomeIcon, MagnifyingGlassIcon, MapIcon, PlusCircleIcon, UserI
 
 const FooterNavi = () => {
     return (
-        <div className="bg-white h-15 fixed inset-x-0 bottom-0 w-full flex justify-center place-items-center p-2
+        <div className="bg-white h-15 fixed inset-x-0 bottom-0 w-full flex justify-center place-items-center p-1
         space-x-7 shadow-md md:hidden
-        border-t-1 border-gray-50">
-            <Link href={"/"}><HomeIcon className="h-10 text-gray-400 hover:scale-95 duration-300" /></Link>
-            <Link href={"/search"}><MagnifyingGlassIcon className='h-10 text-gray-400 hover:scale-95 duration-300' /></Link>
-            <Link href={"/review"}><PlusCircleIcon className='h-10 text-gray-400 hover:scale-95 duration-300' /></Link>
-            <Link href={"/notice"}><BellIcon className='h-10 text-gray-400 hover:scale-95 duration-300' /></Link>
-            <Link href={"/user/profile"}><UserIcon className='h-10 text-gray-400 hover:scale-95 duration-300' /></Link>
+        border-t-2 border-gray-100">
+            <Link href={"/"}><HomeIcon className="h-10 text-[#e0d5c1] hover:scale-95 duration-300" /></Link>
+            {/* <Link href={"/search"}><MagnifyingGlassIcon className='h-10 text-gray-300 hover:scale-95 duration-300' /></Link> */}
+            <Link href={"/review"}><PlusCircleIcon className='h-10 text-[#e0d5c1] hover:scale-95 duration-300' /></Link>
+            {/* <Link href={"/notice"}><BellIcon className='h-10 text-gray-300 hover:scale-95 duration-300' /></Link> */}
+            <Link href={"/user/profile"}><UserIcon className='h-10 text-[#e0d5c1] hover:scale-95 duration-300' /></Link>
         </div>
     )
 }
