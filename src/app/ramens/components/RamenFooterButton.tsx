4c1b435b4f490f1106876be1215b17600305689d
{cocoide} <{cocoide.com@gmail.com}>
2023-01-02 01:56:55 +0900

    Change design at ramen footer

--- src/app/ramens/components/RamenFooterButton.tsx
+++ src/app/ramens/components/RamenFooterButton.tsx
@@ -1,24 +1,25 @@
 "use clinet"
 
-import { HandThumbUpIcon } from '@heroicons/react/24/outline'
-import Image from 'next/image'
+import { HandThumbUpIcon, PencilSquareIcon, ShareIcon } from '@heroicons/react/24/outline'
 
 const RamenFooterButton = () => {
     return (
         <div className="inline-flex justify-center md:hidden w-full">
 
             <button className="flex justify-center place-items-center rounded-xl
-            ring-2 ring-[#c3b9a8] w-[50%] ml-3 my-3 p-2 bg-white text-[#c3b9a8] font-bold"
-            >
-                {/* <Image src="/ramen-footer/ramen.svg" alt="" width={30} height={30} className="h-auto aspect-square mr-2"
-                /> */}
-                <HandThumbUpIcon className="mr-2 w-7 h-7 text-[#c3b9a8]" />
-                クイタイ</button>
+            ring-2 ring-[#c3b9a8] w-[50%] m-3 p-2 bg-white text-[#c3b9a8] font-bold"
+            ><HandThumbUpIcon className="mr-2 w-7 h-7 text-[#c3b9a8]" />
+                保存</button>
 
-            <button className="flex justify-center place-items-center
-            rounded-xl bg-[#c3b9a8]  text-white w-[50%] m-3 p-2 font-bold"
-            ><Image src="/ramen-footer/pen.svg" alt="" width={28} height={28} className="h-auto aspect-square mr-2"
-                />らあ活を投稿</button>
+            <button className="flex justify-center place-items-center rounded-xl
+            ring-2 ring-[#c3b9a8] w-[50%] my-3 p-2 bg-white text-[#c3b9a8] font-bold"
+            ><PencilSquareIcon className="mr-2 w-7 h-7 text-[#c3b9a8]" />
+                投稿</button>
+
+            <button className="flex justify-center place-items-center rounded-xl
+            ring-2 ring-[#c3b9a8] w-[50%] m-3 p-2 bg-white text-[#c3b9a8] font-bold"
+            ><ShareIcon className="mr-2 w-7 h-7 text-[#c3b9a8]" />
+                共有</button>
         </div>
     )
 }
