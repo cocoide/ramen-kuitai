aca6d742fc14cf096c857d4308ae68cc1ef45aa5
{cocoide} <{cocoide.com@gmail.com}>
2023-01-16 02:18:55 +0900

    Feat category search

--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -1,22 +1,36 @@
 import Image from 'next/image';
-import RamenCarousel from '../@Components/Animations/RamenCarousel';
-
-
-export default function SearchPage() {
-
+import { cache } from 'react';
+import prisma from '../../libs/client/prisma';
+const getCategory = cache(async () => {
+    const res = await prisma.category.findMany()
+    return res
+})
+export default async function SearchPage() {
+    const category = await getCategory()
     return (
         <>
             <div className="flex">
-                <div className="flex overflow-hidden overflow-x-auto snap-x snap-mandatory">
-                    <Image src="/ramens/0.jpeg" alt="" width={600} height={100} className="snap-start w-[100%]" />
-                    <Image src="/ramens/1.jpeg" alt="" width={600} height={100} className="snap-center w-[100%]" />
-                    <Image src="/ramens/3.jpeg" alt="" width={600} height={100} className="snap-center w-[100%]" />
-                    <Image src="/ramens/3.jpeg" alt="" width={600} height={100} className="snap-center w-[100%]" />
-                    <Image src="/ramens/1.jpeg" alt="" width={600} height={100} className="snap-end w-[100%]" />
+                <div className="flex overflow-hidden overflow-x-auto snap-x snap-mandatory brightness-80">
+                    <Image src="/ramens/4.jpeg" alt="" width={800} height={100} className="snap-center w-[100%] md:w-[40%]  h-[450px]" />
+                    <Image src="/ramens/1.jpeg" alt="" width={800} height={100} className="snap-center w-[100%] md:w-[40%]  h-[450px]" />
+                    <Image src="/ramens/4.jpeg" alt="" width={800} height={100} className="snap-center w-[100%] md:w-[40%]  h-[450px]" />
+                    <Image src="/ramens/1.jpeg" alt="" width={800} height={100} className="snap-center w-[100%] md:w-[40%]  h-[450px]" />
+                    <Image src="/ramens/3.jpeg" alt="" width={800} height={100} className="snap-center w-[100%] md:w-[40%]  h-[450px]" />
+                    <Image src="/ramens/3.jpeg" alt="" width={800} height={100} className="snap-center w-[100%] md:w-[40%]  h-[450px]" />
+                    <Image src="/ramens/1.jpeg" alt="" width={800} height={100} className="snap-center w-[100%] md:w-[40%]  h-[450px]" />
+                    <Image src="/ramens/2.jpeg" alt="" width={800} height={100} className="snap-center w-[100%] md:w-[40%]  h-[450px]" />
+                    <Image src="/ramens/4.jpeg" alt="" width={800} height={100} className="snap-center w-[100%] md:w-[40%]  h-[450px]" />
                 </div>
             </div>
-            <div className="h-screen">
-
+            <div className="">
+                <div className="grid grid-cols-3 gap-3 p-5">
+                    {category.map((c) => {
+                        return <div className="p-3 w-full place-center bg-secondary rounded-xl text-gray-800 ring-1 ring-primary ring-offset-2" key={c.id}>
+                            {c.name}
+                        </div>
+                    }
+                    )}
+                </div>
             </div>
         </>
     )
